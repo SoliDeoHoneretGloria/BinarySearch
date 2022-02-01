6e987b14a8d9f8d438ce56e58e598da70f245e58
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-02-01 16:00:52 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var values []int = []int{17, 28, 43, 67, 88, 92, 100}
+	target := 43
+	min := 0
+	max := len(values)
+
+	loc := binSearch(target, values...)
+	fmt.Println(loc)
+
+	locR := binSearchRec(min, max, target, values...)
+	fmt.Println(locR)
+}
+
+func binSearch(target int, i ...int) int {
+	min := 0
+	max := len(i)
+	mid := 0
+
+	for min = 0; min <= max; {
+		mid = int(math.Round(float64((min + max) / 2)))
+		if i[mid] == target {
+			break
+		} else if i[mid] < target {
+			min = mid + 1
+		} else {
+			max = mid - 1
+		}
+	}
+	return mid
+}
+
+func binSearchRec(min, max, target int, i ...int) int {
+	mid := int(math.Round(float64((min + max) / 2)))
+	if i[mid] == target {
+		return mid
+	} else if i[mid] < target {
+		return binSearchRec(mid+1, max, target, i...)
+	} else {
+		return binSearchRec(min, mid-1, target, i...)
+	}
+
+}
