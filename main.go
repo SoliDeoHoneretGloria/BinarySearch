154f524946a1657565e334248542bd8bd4021f58
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-02-01 16:30:04 +0100

    Second Commit

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var values []int = []int{17, 28, 43, 67, 88, 92, 100}
-	target := 43
+	target := 42
 	min := 0
 	max := len(values)
 
@@ -25,22 +25,22 @@ func binSearch(target int, i ...int) int {
 
 	for min = 0; min <= max; {
 		mid = int(math.Round(float64((min + max) / 2)))
-		if min > max {
-			return -1
-		}
 		if i[mid] == target {
-			break
+			return mid
 		} else if i[mid] < target {
 			min = mid + 1
 		} else {
 			max = mid - 1
 		}
 	}
-	return mid
+	return -1
 }
 
 func binSearchRec(min, max, target int, i ...int) int {
 	mid := int(math.Round(float64((min + max) / 2)))
+	if min > max {
+		return -1
+	}
 	if i[mid] == target {
 		return mid
 	} else if i[mid] < target {
